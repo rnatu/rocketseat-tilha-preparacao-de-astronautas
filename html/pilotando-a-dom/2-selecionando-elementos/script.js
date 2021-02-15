9e309f942efb3fe91c6622be9be2b49cec912306
Renato Xavier <rnatu91@gmail.com>
2021-02-15 16:33:09 -0300

    HTML: Selecionando elementos finalizado

--- html/pilotando-a-dom/2-selecionando-elementos/script.js
+++ html/pilotando-a-dom/2-selecionando-elementos/script.js
@@ -1,22 +1,26 @@
 // getElementById() -> busca pelo nome do id
+// retorna o element
 const element1 = document.getElementById('blog-title')
 console.log('getElementById')
 console.log(element1)
 console.log('')
 
 // getElementsByClassName() -> busca pelo nome da class
+// retorna um HTMLCollection
 const element2 = document.getElementsByClassName('one')
 console.log('getElementsByClassName')
 console.log(element2)
 console.log('')
 
 // getElementsByTagName() -> busca pelo nome da tag
+// retorna um HTMLCollection
 const element3 = document.getElementsByTagName('h1');
 console.log('getElementsByTagName')
 console.log(element3)
 console.log('')
 
 // querySelector() -> busca pelo seletor
+// retorna o element
 const element4 = document.querySelector('[src]');
 console.log('uerySelector')
 console.log(element4)
