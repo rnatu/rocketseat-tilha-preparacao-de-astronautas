9dafe32a92b6da80bb6b7ecf20a24c1e370012e3
Renato Xavier <rnatu91@gmail.com>
2021-02-15 18:35:39 -0300

    HTML: innerText

--- html/pilotando-a-dom/3-manipulando-conteudos/script.js
+++ html/pilotando-a-dom/3-manipulando-conteudos/script.js
@@ -3,3 +3,12 @@ const element1 = document.querySelector('h1')
 console.log(element1.textContent)
 element1.textContent = 'Olá Devs!'
 element1.textContent += ' Seja Bem-vindo(a)'
+console.log('')
+
+// innerText
+const element2 = document.querySelector('h2');
+console.log(element2.innerText)
+element2.innerText = 'Alterado'
+element2.innerText += ' Texto concatenado'
+console.log('')
+
