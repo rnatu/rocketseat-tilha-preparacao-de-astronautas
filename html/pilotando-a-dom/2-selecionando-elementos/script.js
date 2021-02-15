cea6709b42d88d68d333563d4f05cf07909501aa
Renato Xavier <rnatu91@gmail.com>
2021-02-15 16:20:10 -0300

    HTML: querySelector

--- html/pilotando-a-dom/2-selecionando-elementos/script.js
+++ html/pilotando-a-dom/2-selecionando-elementos/script.js
@@ -1,11 +1,15 @@
-// getElementById()
+// getElementById() -> busca pelo nome do id
 const element1 = document.getElementById('blog-title')
 console.log(element1)
 
-// getElementsByClassName()
+// getElementsByClassName() -> busca pelo nome da class
 const element2 = document.getElementsByClassName('one')
 console.log(element2)
 
-// getElementsByTagName()
+// getElementsByTagName() -> busca pelo nome da tag
 const element3 = document.getElementsByTagName('h1');
 console.log(element3)
+
+// querySelector() -> busca pelo seletor
+const element4 = document.querySelector('[src]');
+console.log(element4)
