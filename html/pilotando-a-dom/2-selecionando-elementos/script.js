67246ea09fc413af016f656903d895489e1196e5
Renato Xavier <rnatu91@gmail.com>
2021-02-14 21:33:47 -0300

    HTML: getElementsByTagName

--- html/pilotando-a-dom/2-selecionando-elementos/script.js
+++ html/pilotando-a-dom/2-selecionando-elementos/script.js
@@ -2,6 +2,9 @@
 const element1 = document.getElementById('blog-title')
 console.log(element1)
 
-// getElementByClassName()
+// getElementsByClassName()
 const element2 = document.getElementsByClassName('one')
 console.log(element2)
+
+const element3 = document.getElementsByTagName('h1');
+console.log(element3)
