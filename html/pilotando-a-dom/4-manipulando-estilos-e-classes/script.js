7a79aa755a5a8d8a26c63edf5597bc623b630030
Renato Xavier <rnatu91@gmail.com>
2021-02-16 14:55:38 -0300

    HTML: style

---
+++ html/pilotando-a-dom/4-manipulando-estilos-e-classes/script.js
@@ -0,0 +1,9 @@
+// style.atributCss -> é utilizado o estilo camelCase
+const element1 = document.querySelector('body')
+
+// setando valor
+element1.style.backgroundColor = '#f9f3d2';
+
+//obtendo valor
+const bodyBgCollor = element1.style.backgroundColor
+console.log(bodyBgCollor)
