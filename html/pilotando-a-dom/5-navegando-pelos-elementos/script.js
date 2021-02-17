0a1c897b4a03c6066bc89dd852a2c785bdccaa5d
Renato Xavier <rnatu91@gmail.com>
2021-02-17 16:17:48 -0300

    HTML: pegando elementos filhos

--- html/pilotando-a-dom/5-navegando-pelos-elementos/script.js
+++ html/pilotando-a-dom/5-navegando-pelos-elementos/script.js
@@ -1,4 +1,30 @@
-// parentNode parentElement
-const element1 = document.querySelector('h1');
-console.log(element1.parentElement)
-console.log(element1.parentNode)
+//# parentNode e parentElement
+const el1 = document.querySelector('h1');
+console.log(el1.parentElement)
+console.log(el1.parentNode)
+console.log('')
+
+//# elementos filhos
+//% childNodes (NodeList) e children (HTMLCollection)
+const el2 = document.querySelector('body')
+
+console.log(el2.childNodes) //NodeList
+//? leva em consideração elementos vazios
+
+console.log(el2.children) // HTMLCollection
+console.log('')
+
+//% firstChild e firstElementChild
+console.log(el2.firstChild) //NodeList
+//? leva em consideração elementos vazios
+
+console.log(el2.firstElementChild) // HTMLCollection
+console.log('')
+
+//% lastChild e lastElementChild
+console.log('lastChild e lastElementChild')
+console.log(el2.lastChild) //NodeList
+//? leva em consideração elementos vazios
+
+console.log(el2.lastElementChild) // HTMLCollection
+console.log('')
