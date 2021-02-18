33d6218c948671bbae0a31b04e22ca3d7468ddf7
Renato Xavier <rnatu91@gmail.com>
2021-02-18 16:50:30 -0300

    Update modal.js

--- html/pilotando-a-dom/8-exercicio/modal.js
+++ html/pilotando-a-dom/8-exercicio/modal.js
@@ -3,11 +3,11 @@ const button = document.querySelector('#openModal');
 const modal = document.querySelector('.modal-wrapper')
 
 button.addEventListener('click', function() {
-  modal.classList.toggle('invisible');
+  modal.classList.remove('invisible');
 })
 
 document.addEventListener('keydown', event => {
   const isEscKey = event.key === 'Escape'
 
-  if(isEscKey && !modal.classList.contains('invisible')) modal.classList.toggle('invisible');
+  if(isEscKey && !modal.classList.contains('invisible')) modal.classList.add('invisible');
 })
